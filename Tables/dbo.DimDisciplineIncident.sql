91973e6bd9344781a85a5ec4c3c29869429d139c
Emilio Baez <emilio@baezconsulting.io>
2020-06-19 09:09:17 -0400

    Removed sourceKey from DimDisciplineIncident

--- Tables/dbo.DimDisciplineIncident.sql
+++ Tables/dbo.DimDisciplineIncident.sql
@@ -1,7 +1,6 @@
 CREATE TABLE [dbo].[DimDisciplineIncident]
 (
 [DisciplineIncidentKey] [int] NOT NULL IDENTITY(1, 1),
-[_sourceKey] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [SchoolKey] [int] NOT NULL,
 [ShortNameOfInstitution] [nvarchar] (500) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [NameOfInstitution] [nvarchar] (500) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
