3f0514ab7216df330bcca93e946dd152b79743f4
Emilio Baez <emilio@baezconsulting.io>
2020-06-30 18:50:24 -0400

    renamed student course grades fact

---
+++ Tables/dbo.FactStudentCourseTranscript.sql
@@ -0,0 +1,25 @@
+CREATE TABLE [dbo].[FactStudentCourseTranscript]
+(
+[StudentKey] [int] NOT NULL,
+[TimeKey] [int] NOT NULL,
+[CourseKey] [int] NOT NULL,
+[SchoolKey] [int] NOT NULL,
+[EarnedCredits] [int] NOT NULL,
+[PossibleCredits] [int] NOT NULL,
+[FinalLetterGradeEarned] [nvarchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[FinalNumericGradeEarned] [decimal] (9, 2) NULL,
+[LineageKey] [int] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[FactStudentCourseTranscript] ADD CONSTRAINT [PK_FactStudentCourseTranscript] PRIMARY KEY CLUSTERED  ([StudentKey], [TimeKey], [CourseKey]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[FactStudentCourseTranscript] ADD CONSTRAINT [FK_FactStudentCourseTranscript_CourseKey] FOREIGN KEY ([CourseKey]) REFERENCES [dbo].[DimCourse] ([CourseKey])
+GO
+ALTER TABLE [dbo].[FactStudentCourseTranscript] ADD CONSTRAINT [FK_FactStudentCourseTranscript_LineageKey] FOREIGN KEY ([LineageKey]) REFERENCES [dbo].[Lineage] ([LineageKey])
+GO
+ALTER TABLE [dbo].[FactStudentCourseTranscript] ADD CONSTRAINT [FK_FactStudentCourseTranscript_SchoolKey] FOREIGN KEY ([SchoolKey]) REFERENCES [dbo].[DimSchool] ([SchoolKey])
+GO
+ALTER TABLE [dbo].[FactStudentCourseTranscript] ADD CONSTRAINT [FK_FactStudentCourseTranscript_StudentKey] FOREIGN KEY ([StudentKey]) REFERENCES [dbo].[DimStudent] ([StudentKey])
+GO
+ALTER TABLE [dbo].[FactStudentCourseTranscript] ADD CONSTRAINT [FK_FactStudentCourseTranscript_TimeKey] FOREIGN KEY ([TimeKey]) REFERENCES [dbo].[DimTime] ([TimeKey])
+GO
