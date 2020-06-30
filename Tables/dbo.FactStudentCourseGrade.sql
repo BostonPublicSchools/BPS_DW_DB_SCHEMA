ddbcd8a8aa828e5e429eb268f7e463f519ea075b
Emilio Baez <emilio@baezconsulting.io>
2020-06-30 07:33:19 -0400

    Added stateschoolcode to DimSchool and Added SchoolKey to FactStudentCourseGrade

--- Tables/dbo.FactStudentCourseGrade.sql
+++ Tables/dbo.FactStudentCourseGrade.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[FactStudentCourseGrade]
 [StudentKey] [int] NOT NULL,
 [TimeKey] [int] NOT NULL,
 [CourseKey] [int] NOT NULL,
+[SchoolKey] [int] NOT NULL,
 [CreditsEarned] [int] NOT NULL,
 [CreditsPossible] [int] NOT NULL,
 [FinalMark] [nvarchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
@@ -14,6 +15,8 @@ GO
 ALTER TABLE [dbo].[FactStudentCourseGrade] ADD CONSTRAINT [FK_FactStudentCourseGrade_CourseKey] FOREIGN KEY ([CourseKey]) REFERENCES [dbo].[DimCourse] ([CourseKey])
 GO
 ALTER TABLE [dbo].[FactStudentCourseGrade] ADD CONSTRAINT [FK_FactStudentCourseGrade_LineageKey] FOREIGN KEY ([LineageKey]) REFERENCES [dbo].[Lineage] ([LineageKey])
+GO
+ALTER TABLE [dbo].[FactStudentCourseGrade] ADD CONSTRAINT [FK_FactStudentCourseGrade_SchoolKey] FOREIGN KEY ([SchoolKey]) REFERENCES [dbo].[DimSchool] ([SchoolKey])
 GO
 ALTER TABLE [dbo].[FactStudentCourseGrade] ADD CONSTRAINT [FK_FactStudentCourseGrade_StudentKey] FOREIGN KEY ([StudentKey]) REFERENCES [dbo].[DimStudent] ([StudentKey])
 GO
